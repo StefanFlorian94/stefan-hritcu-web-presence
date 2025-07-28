ee74413b42fb4b046f06c364e1ba3d4be652fbf7
StefanFlorian94 <official.stefanmail@gmail.com>
2025-07-28 21:37:19 +0200

    add: first configuration

---
+++ frontend/src/app/components/layouts/header/Header.tsx
@@ -0,0 +1,11 @@
+export default function Header() {
+  return (
+    <>
+      <header>
+        <div className="bg-green-400 p-12">
+          <h2 className="text-red-500">LOGO</h2>
+        </div>
+      </header>
+    </>
+  );
+}
