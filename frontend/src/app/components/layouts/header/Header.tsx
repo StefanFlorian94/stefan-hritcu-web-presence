4fa0b60521e4874395d5617c956e110ecab010b5
StefanFlorian94 <official.stefanmail@gmail.com>
2025-07-29 21:17:36 +0200

    feat: initial Header component

--- frontend/src/app/components/layouts/header/Header.tsx
+++ frontend/src/app/components/layouts/header/Header.tsx
@@ -1,9 +1,25 @@
+import Link from "next/link";
+
 export default function Header() {
   return (
     <>
-      <header>
-        <div className="bg-green-400 p-12">
-          <h2 className="text-red-500">LOGO</h2>
+      <header className="mx-auto max-w-screen-2xl px-6 sm:px-12 md:px-20 lg:px-32 xl:px-44 2xl:px-64 h-24 flex items-center justify-between">
+        {/*  LOGO */}
+        <div>
+          <h2 className="text-red-500 text-sm sm:text-base md:text-lg lg:text-xl">
+            LOGO
+          </h2>
+        </div>
+        {/*  NAVBAR & HAMBURGER BUTTON */}
+        <div className="flex items-center justify-around">
+          <nav>
+            <Link href="/">Categories</Link>
+            <Link href="/">About</Link>
+            <Link href="/">Contact</Link>
+          </nav>
+          <div>
+            <h2>+</h2>
+          </div>
         </div>
       </header>
     </>
